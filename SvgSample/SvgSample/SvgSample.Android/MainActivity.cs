e68fc8227ee70d97c5c719c66d03c359efb2467a
Yoshito Tabuchi @ Surface <ytabuchi.xlsoft@gmail.com>
2017-03-24 16:11:08 +0900

    Add Svg init and Delete UWP project

--- SvgSample/SvgSample/SvgSample.Android/MainActivity.cs
+++ SvgSample/SvgSample/SvgSample.Android/MainActivity.cs
@@ -6,6 +6,7 @@ using Android.Runtime;
 using Android.Views;
 using Android.Widget;
 using Android.OS;
+using SVG.Forms.Plugin.Droid;
 
 namespace SvgSample.Droid
 {
@@ -20,6 +21,8 @@ namespace SvgSample.Droid
             base.OnCreate(bundle);
 
             global::Xamarin.Forms.Forms.Init(this, bundle);
+            SvgImageRenderer.Init();
+
             LoadApplication(new App());
         }
     }
--- SvgSample/SvgSample/SvgSample.iOS/AppDelegate.cs
+++ SvgSample/SvgSample/SvgSample.iOS/AppDelegate.cs
@@ -4,6 +4,7 @@ using System.Linq;
 
 using Foundation;
 using UIKit;
+using SVG.Forms.Plugin.iOS;
 
 namespace SvgSample.iOS
 {
@@ -23,6 +24,8 @@ namespace SvgSample.iOS
         public override bool FinishedLaunching(UIApplication app, NSDictionary options)
         {
             global::Xamarin.Forms.Forms.Init();
+            SvgImageRenderer.Init();
+
             LoadApplication(new App());
 
             return base.FinishedLaunching(app, options);
